9184958ef1789df69abde7e4e94fb0b94363cb9d
danielsworthe <256434148+danielsworthe@users.noreply.github.com>
2026-04-10 17:08:37 -0400

    Second API Bronze Level
    Open-Meteo Air Quality API
    Bronze Layer
    Ingest Script

---
+++ assignment3/ingest/ingest_air_quality.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from datetime import datetime
+
+# Toronto
+latitude = 43.65
+longitude = -79.35
+
+url = "https://air-quality-api.open-meteo.com/v1/air-quality"
+
+params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "start_date": "2023-01-01",
+    "timezone": "auto",
+    "end_date": "2023-03-31",
+    "hourly": ["pm10", "pm2_5", "us_aqi"]
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+
+filename = f"data/bronze/air_quality_{datetime.now().strftime('%Y%m%d')}.json"
+
+with open(filename, "w") as f:
+    json.dump(data, f)
+
+print("Air quality data saved to Bronze")
